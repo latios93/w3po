59c6faae9f410510acfb51384fa13ab1518df145
latios93 <latios93@users.noreply.github.com>
2025-11-20 10:04:15 +0100

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,3 +1,22 @@
 HTML = """
-Whatever you want
+This is my operating room, so you all need to shut up.
+It is illegal to buy and sell tigers and other big cats in the United States.
+Vanilla Nesquick is the inferior form of Nesquick.
+She didn't understand why everyone loved his music so much.
+She wished she had bought Bitcoin back in 2010.
+She ripped a big hole in the back of her jeans and didn't notice for hours.
+No burglar would ever have the heart to hurt a pet pig.
+Aluminum helps to keep your food fresh for longer.
+She always worried that sings would embarrass themselves, because if she was in front of a crowd she would always be worried about embarrassing herself.
+It is one of the biggest summer music festivals.
+I don't like the polluted atmosphere of big cities.
+They had to decide between buying diapers and buying ramen to feed themselves.
+At the coffee shop, she couldn't tell if they were annoyed with her or not.
+Judith Jarvis Thompson is the best philosopher of all time.
+The mailbox was bent and broken and looked like someone had knocked it over on purpose.
+He needed a new helmet because his got smashed when he was riding his bike.
+If you could go anywhere on vacation, where would you go?
+Let's all just take a moment to breathe, please!
+These are the things that I want from a relationship.
+Would you like your cash in tens or twenties?
 """
